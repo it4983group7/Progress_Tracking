9d27e53f69377c3c5afcfb802e3fd322959644f1
Quinn Freas <qfreas@students.kennesaw.edu>
2019-03-02 11:39:10 -0500

    update.php record fix

--- php/update.php
+++ php/update.php
@@ -4,21 +4,30 @@ $username = 'web';
 $password = 'Capst0n3!';
 $dbname = 'CAPSTONE';
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+try {
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    // Create connection
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+    
+    $sponsor_update_id = $_POST["sponsor-update-id"];
+    $sponsor_id = $_POST["sponsor-id"];
+    $date = $_POST["date"];
+    $progress = $_POST["progress"];
+    $responsiveness = $_POST["responsiveness"];
+    $feedback = $_POST["feedback"];
+    
+    $sql = "INSERT INTO Sponsor_Update(Sponsor_Update_ID, Sponsor_ID, Date, Progress, Responsiveness, Feedback) VALUES ($sponsor_update_id, $sponsor_id, $date, $progress, $responsiveness, $feedback)";
+    
+    $conn->exec($sql);
+    echo "New record created successfully";
 }
-
-$sponsor_update_id = $_POST["sponsor-update-id"];
-$sponsor_id = $_POST["sponsor-id"];
-$date = $_POST["date"];
-$progress = $_POST["progress"];
-$responsiveness = $_POST["responsiveness"];
-$feedback = $_POST["feedback"];
-
-$sql = "INSERT INTO Sponsor_Update(Sponsor_Update_ID, Sponsor_ID, Date, Progress, Responsiveness, Feedback) VALUES ($sponsor_update_id, $sponsor_id, $date, $progress, $responsiveness, $feedback)";
+catch(Exception $e) {
+    echo $e->getMessage();
+}
 
 $conn->close();
 ?>
