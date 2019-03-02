8440087131df29c5dad5a54833990193a406fea4
Quinn Freas <qfreas@students.kennesaw.edu>
2019-03-01 21:50:18 -0500

    php form for update

--- php/update.php
+++ php/update.php
@@ -11,8 +11,14 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
+$sponsor_update_id = $_POST["sponsor-update-id"];
+$sponsor_id = $_POST["sponsor-id"];
+$date = $_POST["date"];
+$progress = $_POST["progress"];
+$responsiveness = $_POST["responsiveness"];
+$feedback = $_POST["feedback"];
 
-$sql = "";
+$sql = "INSERT INTO Sponsor_Update(Sponsor_Update_ID, Sponsor_ID, Date, Progress, Responsiveness, Feedback) VALUES ($sponsor_update_id, $sponsor_id, $date, $progress, $responsiveness, $feedback)";
 
 $conn->close();
 ?>
