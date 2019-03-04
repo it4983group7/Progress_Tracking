a2dcc635a78e6effddd8b085875cec069ff4d339
Quinn Freas <qfreas@students.kennesaw.edu>
2019-03-03 22:46:22 -0500

    Update der Seite korrigieren. PHP-Funktionalität behoben.

--- php/update.php
+++ php/update.php
@@ -1,9 +1,10 @@
 <?php
+echo file_get_contents("../nav.html");
+
 $servername = 'localhost';
 $username = 'web';
 $password = 'Capst0n3!';
 $dbname = 'capstone';
-
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
@@ -13,15 +14,17 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-$sponsor_update_id = $_POST["sponsor-update-id"];
+// $sponsor_update_id = $_POST["sponsor-update-id"];
+$sponsor_update_id = date('YmdHis');
 $sponsor_id = $_POST["sponsor-id"];
 $project_id = $_POST["project-id"];
-$date = $_POST["date"];
+// $date = $_POST["date"];
+$date = date('Y-m-d H:i:s');
 $progress = $_POST["progress"];
 $responsiveness = $_POST["responsiveness"];
 $feedback = $_POST["feedback"];
 
-$sql = "INSERT INTO Sponsor_Update(Sponsor_Update_ID, Sponsor_ID, Project_ID, Progress, Responsiveness, Feedback) VALUES ($sponsor_update_id, $sponsor_id, $project_id, $progress, $responsiveness, \"$feedback\")";
+$sql = "INSERT INTO Sponsor_Update(Sponsor_Update_ID, Sponsor_ID, Project_ID, Date, Progress, Responsiveness, Feedback) VALUES ($sponsor_update_id, $sponsor_id, $project_id, \"$date\", $progress, $responsiveness, \"$feedback\")";
 
 if ($conn->query($sql) === TRUE) {
     echo "New record created successfully";
