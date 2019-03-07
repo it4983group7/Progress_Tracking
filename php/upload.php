48c1061e14329633e1e561a864214b69806e5d56
Quinn Freas <qfreas@students.kennesaw.edu>
2019-03-07 17:39:42 -0500

    attempt at upload

--- php/upload.php
+++ php/upload.php
@@ -1,16 +1,35 @@
 <?php
-$target_dir = "/uploads/";
+$target_dir = "uploads/";
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 $uploadOk = 1;
 $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
-
-// Allow certain file formats
-if($imageFileType != "xls" && $imageFileType != "xlsb" && $imageFileType != "xlsm" && $imageFileType != "xlsx" ) {
-    echo "Sorry, only XLS, XLSB, XLSM & XLSX files are allowed.";
+// Check if image file is a actual image or fake image
+if(isset($_POST["submit"])) {
+    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+    if($check !== false) {
+        echo "File is an image - " . $check["mime"] . ".";
+        $uploadOk = 1;
+    } else {
+        echo "File is not an image.";
+        $uploadOk = 0;
+    }
+}
+// Check if file already exists
+if (file_exists($target_file)) {
+    echo "Sorry, file already exists.";
     $uploadOk = 0;
 }
-
-// echo exec('whoami');
+// // Check file size
+// if ($_FILES["fileToUpload"]["size"] > 500000) {
+//     echo "Sorry, your file is too large.";
+//     $uploadOk = 0;
+// }
+// Allow certain file formats
+if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+&& $imageFileType != "gif" ) {
+    echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+    $uploadOk = 0;
+}
 // Check if $uploadOk is set to 0 by an error
 if ($uploadOk == 0) {
     echo "Sorry, your file was not uploaded.";
