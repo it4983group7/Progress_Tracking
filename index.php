c5afec3fbcfacaf4783efeeb28885a81af72c415
branyang1 <47230457+branyang1@users.noreply.github.com>
2019-03-14 22:56:53 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,15 @@
+<?php
+echo file_get_contents("../nav.html");
 
+$servername = 'localhost';
+$username = 'web';
+$password = 'Capst0n3!';
+$dbname = 'capstone';
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Create connection
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
